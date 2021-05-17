e180b823cee05a430ca99197520c1a6a5d019d44
MayronSouza <mayron.souza@gmz.solutions>
2021-05-17 17:18:40 -0300

    refactoring the Home, moving the style for a styles variable using StylesSheet of the react-native package

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export const Home= () => {
  return (
   <View
-    style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}
+    style={styles.container}
   >
     <Text>React Native</Text>
     <Text>Ignite</Text>
   </View>
  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
