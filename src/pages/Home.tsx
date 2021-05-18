2adc534a6436feb2877bd1d6756656168e800009
MayronSouza <mayron.souza@gmz.solutions>
2021-05-18 16:54:32 -0300

    including the TypeScript into the Home page, inserting the Skill interface

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,9 +11,14 @@ import {
 import { Button } from '../components/Button';
 import { SkillCard } from '../components/SkillCard';
 
+interface SkillData {
+  id: string;
+  name:string;
+}
+
 export const Home = () => {
   const [newSkill, setNewSkill] = useState('');
-  const [mySkills, setMySkills] = useState([]);
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
   const [greeting, setGreeting] = useState('');
 
   useEffect(() => {
@@ -28,10 +33,16 @@ export const Home = () => {
     }
   }, []);
 
-  const handleAddNewSkill = () => setMySkills(
-    oldState => [...oldState, newSkill]
-  );
+  const handleAddNewSkill = () => {
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill,
+    }
 
+    setMySkills(
+      oldState => [...oldState, data]
+    );
+  }
  return (
   <View style={styles.container}>
     <Text style={styles.title}>Welcome, Mayron Souza</Text>
@@ -45,16 +56,11 @@ export const Home = () => {
     <Button onPress={handleAddNewSkill}/>
     <FlatList
       data={mySkills}
-      keyExtractor={item => item}
+      keyExtractor={item => item.id}
       renderItem={({ item }) => (
-        <SkillCard skill={item}/>
+        <SkillCard skill={item.name}/>
       )}
     />
-    {/* {
-      mySkills.map((skill, index) => (
-        
-      ))
-    } */}
   </View>
  );
 }
