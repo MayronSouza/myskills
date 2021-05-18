0a7c0d733104d0e76f6888646c416634b1a2671a
MayronSouza <mayron.souza@gmz.solutions>
2021-05-17 21:09:59 -0300

    customizing the Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
 
 export const Home= () => {
  return (
-  <View
-    style={styles.container}
-  >
-    <Text>React Native</Text>
-    <Text>Ignite</Text>
+  <View style={styles.container}>
+    <Text style={styles.title}>Welcome, Mayron Souza</Text>
+    <TextInput
+      style={styles.input}
+      placeholder='New Skill...'
+      placeholderTextColor='#555'
+    />
+    <TouchableOpacity style={styles.btn}>
+      <Text style={styles.btnText}>Add</Text>
+    </TouchableOpacity>
+    <Text style={styles.skills}>My Skills</Text>
   </View>
  );
 }
@@ -15,7 +28,39 @@ export const Home= () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: '#121015',
+    paddingHorizontal: 20,
+    paddingVertical: Platform.OS === 'ios' ? 70 : 45,
   },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    fontSize: 20,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 32,
+    borderRadius: 8,
+  },
+  btn: {
+    alignItems: 'center',
+    backgroundColor: '#a370f7',
+    padding: 16,
+    borderRadius: 8,
+    marginTop: 24,
+  },
+  btnText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  skills: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 50,
+  }
 });
